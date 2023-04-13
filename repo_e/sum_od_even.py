6c404cc08ce7929c5bbe302ae3b84b04fc442c06
patka9797 <114613048+patka9797@users.noreply.github.com>
2023-04-13 18:46:07 +0200

    Update sum_od_even.py
    Implement build sum()

--- repo_e/sum_od_even.py
+++ repo_e/sum_od_even.py
@@ -1,9 +1,17 @@
+# def sum_of_even(integer):
+#     sum=0
+#     for number in integer:
+#         if number%2==0:
+#             sum += number
+#     return sum
+# print(sum_of_even([1,10,4,3]))
+
 def sum_of_even(integer):
-    sum = 0
+    Even=[]
     for number in integer:
-        if number % 2 == 0:
-            sum += number
-    return sum
-
-
-print(sum_of_even([1, 10, 4, 3]))
+        if number%2==0:
+            Even.append(number)
+    return sum(Even)
+        
+    
+print(sum_of_even([1,10,4,3]))
