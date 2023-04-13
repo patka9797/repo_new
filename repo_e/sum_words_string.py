ef790f3f6c6568345c222c77d8c02ddf9a95e4bd
patka9797 <114613048+patka9797@users.noreply.github.com>
2023-04-13 18:30:39 +0200

    Update sum_words_string.py
    Trying Counter and rename varieable

--- repo_e/sum_words_string.py
+++ repo_e/sum_words_string.py
@@ -1,12 +1,18 @@
+# def sum_word_in_string(string):
+#     counts=dict()
+#     words=str.split(string)
+#     for word in words:
+#         if word in counts:
+#             counts[word] +=1
+#         else: 
+#             counts[word]=1
+#     return counts
+# print(sum_word_in_string('babka, jajko,babka, kurczak'))
+
+from collections import Counter
+
 def sum_word_in_string(string):
-    counts = dict()
-    str_split = str.split(string)
-    for word in str_split:
-        if word in counts:
-            counts[word] += 1
-        else:
-            counts[word] = 1
-    return counts
 
+    print(Counter(string))
 
-print(sum_word_in_string("babka, jajko,babka, kurczak"))
+print(sum_word_in_string('babka, jajko,babka, kurczak'))  
