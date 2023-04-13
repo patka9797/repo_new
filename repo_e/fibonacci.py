5089ffb5c6b3af1e15822c61f5968f68b0924159
patka9797 <114613048+patka9797@users.noreply.github.com>
2023-04-13 19:16:07 +0200

    Update fibonacci.py
    Try to create geberator with yield

--- repo_e/fibonacci.py
+++ repo_e/fibonacci.py
@@ -1,13 +1,16 @@
 def fibonacci():
-    numbers = int(input("How many numbers print?"))
-    n1, n2 = 0, 1
-    count = 0
-    if numbers <= 0:
-        print("Please enter a positive integer")
-    elif numbers == 1:
-        print(n1)
-    else:
-        print("Fibonacci sequence: ")
+    numbers = 0
+    while True:
+        yield numbers
+        numbers +=1
+        n1, n2 = 0, 1
+        count = 0
+    
+    
+        if numbers == 1:
+            print(n1)
+        else:
+            print("Fibonacci sequence: ")
         while count < numbers:
             print(n1)
             next = n1 + n2
