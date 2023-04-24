a12953891df4c986e9eeacc1c8f32cdc2c93189c
patka9797 <114613048+patka9797@users.noreply.github.com>
2023-04-24 18:17:48 +0200

    Update sum_od_even.py
    Change integer to numbers

--- repo_e/sum_od_even.py
+++ repo_e/sum_od_even.py
@@ -1,14 +1,14 @@
-# def sum_of_even(integer):
+# def sum_of_even(numbers:
 #     sum=0
-#     for number in integer:
+#     for number in numbers:
 #         if number%2==0:
 #             sum += number
 #     return sum
 # print(sum_of_even([1,10,4,3]))
 
-def sum_of_even(integer):
+def sum_of_even(numbers):
     Even=[]
-    for number in integer:
+    for number in numbers:
         if number%2==0:
             Even.append(number)
     return sum(Even)
